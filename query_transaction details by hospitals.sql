dd526e315d19ba3c85dee2dfa233406598a76f58
Amjith <amjith4you@outlook.com>
2026-07-22 22:45:00 +0530

    Add files via upload

---
+++ query_transaction details by hospitals.sql
@@ -0,0 +1,2 @@
+SELECT * FROM healthcare_db.patient_records where patient_records.LocationName = "Evergreen Clinic";
+SELECT * FROM healthcare_db.patient_records where patient_records.LocationName = "Pioneer Clinic";
